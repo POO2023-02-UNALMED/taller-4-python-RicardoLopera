ad8206de29ad32de20b544b5c308ff1a95acf0a7
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-09-04 14:49:10 -0500

    Add files via upload

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,25 +1,25 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = "Grado 12"
+    grado = 'Grado 12'
 
-    def __init__(self, grupo= "grupo predeterminado", asignaturas = None, estudiantes = None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, **kwargs):
-        self._asignaturas = {}
+    def listadoAsignaturas(self,**kwargs):
+        self._asignaturas ={}
         for key in kwargs:
             self._asignaturas[key] = kwargs[key]
 
-    def agregarAlumno(self, alumno, lista = None):
-        if (lista is None):
-            self.listadoAlumnos = [alumno]
+    def agregarAlumno(self, alumno, lista=None):
+        if(lista is None):
+           self.listadoAlumnos = [alumno]
         else:
             lista.append(alumno)
             self.listadoAlumnos = lista
-
+  
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
