39816b69ff3fafc816b6b0b8041c21698120f1be
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-09-03 19:04:57 -0500

    Add files via upload

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,28 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes = None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+    def listadoAsignaturas(self, **kwargs):
+        self._asignaturas = {}
+        for key in kwargs:
+            self._asignaturas[key] = kwargs[key]
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
-
-    # def __str__(self):
-    #     pass
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+    def agregarAlumno(self, alumno, lista = None):
+        if (lista is None):
+            self.listadoAlumnos = [alumno]
+        else:
+            lista.append(alumno)
+            self.listadoAlumnos = lista
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    def __str__(self):
+        return f'Grupo de estudiantes: {self._grupo}'
